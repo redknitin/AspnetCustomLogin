27537f50cd6de696b6d4aab67d383f8f75ae441e
Nitin <redknitin@gmail.com>
2015-03-15 23:27:26 +0400

    Initial baseline

---
+++ PracticeMvc2/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity;
+using PracticeMvc2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticeMvc2.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid && model.Username == "nitin" && model.Password == "reddy")
+            {
+                System.Web.HttpContext.Current.GetOwinContext().Authentication.SignIn(new ClaimsIdentity[] { new ClaimsIdentity(new List<Claim>() { new Claim(ClaimTypes.Name, model.Username) }, DefaultAuthenticationTypes.ApplicationCookie ) });
+                string returnUrl = System.Web.HttpContext.Current.Request.QueryString["ReturnUrl"];
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToRoute(new { Controller="Home" });
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid username or password");
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            System.Web.HttpContext.Current.GetOwinContext().Authentication.SignOut();
+            return RedirectToRoute(new { Controller = "Login", Action = "Index" });
+        }
+    }
+}
---
+++ PracticeMvc2/Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticeMvc2.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool IsPersistent { get; set; }
+    }
+}
